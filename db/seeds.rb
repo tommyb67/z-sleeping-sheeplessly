1a78460f61d41f1a6a70f03190bfef19b27b7888
Tom Brennan <tommybrennan@gmail.com>
2017-02-27 22:01:00 -0500

    created banner seeds in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |banner|
+  Banner.create(start_date: Date.today, end_date: Date.tomorrow, image: "k:/banners/sleeping", location: "Jumbotron 2")
+end
+
+puts "10 Banner requests have been created"
