aa1934e4e218186dffe1b74454bf57506a6e3bd3
Tom Brennan <tommybrennan@gmail.com>
2017-02-28 19:14:13 -0500

    created a new banner form

--- app/controllers/banners_controller.rb
+++ app/controllers/banners_controller.rb
@@ -1,4 +1,20 @@
 class BannersController < ApplicationController
   def index
   end
+
+  def new
+    @banner = Banner.new
+  end
+
+  def create
+    @banner = Banner.new(params.require(:banner).permit(:start_date, :end_date, :image, :location))
+
+    @banner.save
+
+    redirect_to @banner
+  end
+
+  def show
+    @banner = Banner.find(params[:id])
+  end
 end
