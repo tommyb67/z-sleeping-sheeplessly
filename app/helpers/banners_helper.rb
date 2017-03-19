4bccbf97f8a1a227285f213ccbc90100075972b1
Tom Brennan <tommybrennan@gmail.com>
2017-03-19 16:37:05 -0400

    implemented labels with status values for banner submissions

--- app/helpers/banners_helper.rb
+++ app/helpers/banners_helper.rb
@@ -1,2 +1,22 @@
 module BannersHelper
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+    def status_span_generator status
+      case status
+        when 'submitted'
+          content_tag(:span, status.titleize, class: 'label label-primary')
+        when 'assigned'
+          content_tag(:span, status.titleize, class: 'label label-info')
+        when 'completed'
+          content_tag(:span, status.titleize, class: 'label label-warning')
+        when 'rejected'
+          content_tag(:span, status.titleize, class: 'label label-danger')
+        when 'approved'
+          content_tag(:span, status.titleize, class: 'label label-success')
+        end
+    end
 end
